f5c2597257e432eb3f4bb9905867d006ac04a3e0
Sebastian L <99sebastianl@gmail.com>
2024-10-13 14:41:27 -0400

    Troubleshooting custom domain: constants.ts base url

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  window.location.hostname === "paulospoetry.com"
-    ? "/" // for custom domain
-    : "/poetry-website"; // for GitHub Pages
+export const BASE_URL = "/";

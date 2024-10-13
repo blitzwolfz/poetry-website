c15a9582782f90955f0d3036e701e04f90703fd4
Sebastian L <99sebastianl@gmail.com>
2024-10-13 14:31:05 -0400

    Updated vite config again for custom domain

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/poetry-website/" : "/",
+  base: "/", // Set to root for custom domain
 });
